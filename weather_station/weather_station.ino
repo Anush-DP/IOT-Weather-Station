43da2967a7a81d803c2d83a0957f82bd0d202bd3
Anush DP <anushdp.dsce@gmail.com>
2019-04-12 22:00:51 +0530

    Moved to directory

---
+++ weather_station/weather_station.ino
@@ -0,0 +1,58 @@
+/*
+   Copyright (C) 2019 Anush DP
+   License: GPL3
+
+*/
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <BigNumbers_I2C.h>
+#include "IOT_WS.h"
+#include "LCD_DEF.h"
+
+void setup() {
+  Wire.begin(D2,D1); //Remove this line if you are using Arduino
+  lcd.backlight();
+  lcd.begin();
+  createLoad();
+  loading();
+  delay(300);
+  clear1();
+  delay(300);
+  win_welcome();
+  lcd.setCursor(0, 3);
+  delay(1500);
+  lcd.print(F("This isn't Win95 LOL"));
+  delay(1000);
+}
+
+void loop()
+{
+  server.handleClient();
+  bigNum.begin();
+  server.handleClient();
+  displayTemp(Temperature);
+  server.handleClient();
+  delay(4000);
+  server.handleClient();
+  displayHumidity(Humidity);
+  server.handleClient();
+  delay(4000);
+  server.handleClient();
+  displayAQ(400);
+  server.handleClient();
+  delay(4000);
+  server.handleClient();
+  rain(3000);
+  server.handleClient();
+  sun();
+  server.handleClient();
+  delay(4000);
+  server.handleClient();
+  lcd.clear();
+  server.handleClient();
+  lcd.print(F("Credits: DISPLAY ART"));
+  server.handleClient();
+  chineseTextReveal(5,2,"ANUSH DP");  // text should be 8 letters/characters long
+  server.handleClient();
+  delay(2000);
+}
